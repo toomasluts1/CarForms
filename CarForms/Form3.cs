ffe6c5c1134208c416a8eceb1dee4e5f4d6bd0f2
toomasluts1 <toomasluts1@gmail.com>
2019-02-09 11:03:05 +0200

    Open/Save without methods again

---
+++ CarForms/Form3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarForms
+{
+    public partial class Form3 : Form
+    {
+        public Form1 carsForm;
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            tbCarMark.Text = carsForm.getCarMark();
+            tbMaker.Text = carsForm.getMaker();
+            tbType.Text = carsForm.getType();
+            tbReleaseYear.Text = carsForm.getReleaseYear();
+            tbRegistrationDate.Text = carsForm.getRegistrationDate();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            carsForm.Show();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            carsForm.setCarMark(tbCarMark.Text);
+            carsForm.setMaker(tbMaker.Text);
+            carsForm.setType(tbType.Text);
+            carsForm.setReleaseYear(tbReleaseYear.Text);
+            carsForm.setRegistrationDate(tbRegistrationDate.Text);
+
+            this.Close();
+            carsForm.Show();
+        }
+    }
+}
